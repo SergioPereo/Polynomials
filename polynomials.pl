92cecef49bb2dbe4659da93037975912a30f8190
maudiaz221 <98540443+maudiaz221@users.noreply.github.com>
2023-02-13 07:44:40 -0600

    Update polynomials.pl

--- polynomials.pl
+++ polynomials.pl
@@ -116,11 +116,27 @@ to_string(Pol):-
 	print_lot(FilteredLoT),
 	write(LastTerm),nl.
 
+/*El metodo derival es el metodo en el que se ingresa el polinomio inicial
+y regresa como resultado la derivada del polinomio.
+Este mismo calcula el grado del polinomio para saber hasta donde avanzar en el arreglo y lo ingresa
+como parametro en el siguiente metodo y mandamos como parametro la cola para reducir el grado del polinomio ya que la derivada 
+de una constante siempre será 0 por eso desde un inicio ya nos deshacemos de un valor y el nuevo polinomio sera un polinomio con 
+grado menor.*/
+
 deriva([_|Pol], NuevoPol):-
 	length(Pol,N),
 	deriva(Pol,N,N,NuevoPol).
 
+/*El siguiente metodo ya hace el cambio en los coeficientes del polinomio.
+Tiene como caso base que esten vacios los dos polinomios que regrese el metodo deriva
+*/
+
+
 deriva([],_,_,[]):- !.
+
+/*Lo que hace el metodo es recorrer desde el inicio del arreglo con un contador para poder multiplicar los coeficientes del polinomio
+original e ingresarlos al nuevo polinomio siendo contruido y así sucesivamente hasta llegar al caso base
+*/
 
 deriva([A1|Pol],Grado,Cont,[A2|NuevoPol]):-
 	A2 is A1 * (Grado - Cont+1),
